cdb9a9801100c68308422219efa8fdc269c65ad5
Gregory fox <gregfox2@hotmail.co.uk>
2023-02-23 18:21:15 +0000

    Test to ensure returned type is of list user

--- CloudCustomers.UnitTests/Systems/Services/UserServiceTests.cs
+++ CloudCustomers.UnitTests/Systems/Services/UserServiceTests.cs
@@ -2,6 +2,7 @@
 using CloudCustomers.Api.Services;
 using CloudCustomers.UnitTests.Fixtures;
 using CloudCustomers.UnitTests.Helpers;
+using FluentAssertions;
 using Moq;
 using Moq.Protected;
 
@@ -34,6 +35,23 @@ public class UsersServiceTests
             ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
             ItExpr.IsAny<CancellationToken>()
             );
+    }
+
+    [Fact]
+    public async Task GetAllUsers_WhenCalled_ReturnsListOfUsers()
+    {
+        // Arrange
+        var expectedResponse = UsersFixture.GetTestUsers();
+
+        var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
+        var httpClient = new HttpClient(handlerMock.Object);
+        var sut = new UsersService(httpClient);
+
+        // Act
+        var result = await sut.GetAllUsers();
+
+        // Assert
+        result.Should().BeOfType<List<User>>();
     }
 }
 
